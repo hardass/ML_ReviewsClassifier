65be23a1e640bbb5921e5bdbb80ce31101096da8
hardas yang <hardas.yang@gmail.com>
2017-06-21 23:31:36 +0800

    update in 6.21

--- test_MarkWordAsNumber.py
+++ test_MarkWordAsNumber.py
@@ -11,18 +11,14 @@ else:
     # words = collections.defaultdict(int)
     strip = string.whitespace + string.punctuation + string.digits + "\"'"
     for filename in sys.argv[1:]:
-        post_input = []
         for line in open(filename):
-            post_line = []
             for word in line.split():
                 word = word.strip(strip)
                 if len(word) >= 2:
                     if words.get(word, 0) == 0:
                         words[word] = i
                         i = i + 1
-                    post_line.append(words[word])
-            post_input.append(post_line)
-    for line in post_input:
-        print(line)
-    # for word in sorted(words):
-        # print("'{0}' unique number is {1}".format(word, words[word]))
+
+                        # words[word] = words.get(word, 0) + 1\
+    for word in sorted(words):
+        print("'{0}' occurs {1} times".format(word, words[word]))
