c928a208b4873b3013d3f2f9d99ff245f08f2215
Yang <hardas-yang@MERCER.COM>
2017-06-22 10:16:43 +0800

    null

--- test_reader.py
+++ test_reader.py
@@ -1,20 +1,28 @@
-import sys  
-import string  
-#import collections  
-  
-if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:  
- print("usage: uniqueword filename_1 filename_2 ... filename_n")  
- sys.exit()  
-else:  
- words = {}   
- # words = collections.defaultdict(int)  
- strip = string.whitespace + string.punctuation + string.digits + "\"'"  
- for filename in sys.argv[1:]:  
-  for line in open(filename):  
-    for word in line.split():  
-    word = word.strip(strip)  
-    if len(word) >= 2:  
-     words[word] = words.get(word, 0) + 1  
-     # words[word] += 1  
- for word in sorted(words):  
-  print("'{0}' occurs {1} times".format(word,words[word])) 
+import sys
+import string
+#import collections
+
+if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
+    print("usage: uniqueword filename_1 filename_2 ... filename_n")
+    sys.exit()
+else:
+    words = {}
+    i = 0
+    # words = collections.defaultdict(int)
+    strip = string.whitespace + string.punctuation + string.digits + "\"'"
+    for filename in sys.argv[1:]:
+        post_input = []
+        for line in open(filename):
+            post_line = []
+            for word in line.split():
+                word = word.strip(strip)
+                if len(word) >= 2:
+                    if words.get(word, 0) == 0:
+                        words[word] = i
+                        i = i + 1
+                    post_line.append(words[word])
+            post_input.append(post_line)
+    for line in post_input:
+        print(line)
+    # for word in sorted(words):
+        # print("'{0}' unique number is {1}".format(word, words[word]))
